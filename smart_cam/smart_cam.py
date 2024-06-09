73e602b54e0450f84262aa39ea97a5a80b0144c5
tynegr <122130536+tynegr@users.noreply.github.com>
2024-06-09 12:45:25 +0300

    Update smart_cam.py

--- smart_cam/smart_cam.py
+++ smart_cam/smart_cam.py
@@ -1,13 +1,31 @@
 import reflex as rx
-from smart_cam.components.uploader import upload_form
 from smart_cam.state import State
 
 
 def index() -> rx.Component:
-    return rx.center(rx.vstack(
-        upload_form(),
-        rx.text(State.price)
-    ))
+    return rx.vstack(
+        rx.heading("Загрузка файлов", align="center"),
+        rx.upload(
+            rx.text(
+                "Перенесите файлы или нажмите, чтобы загрузить файл",
+                align="center"
+            ),
+            id="upload3",
+            border="1px dotted rgb(107,99,246)",
+            padding="5em",
+        ),
+            rx.button(
+                "Загрузить",
+                on_click=State.handle_upload(
+                    rx.upload_files(
+                        upload_id="upload3",
+                    ),
+                ),
+            ),
+        rx.text(State.price),
+        align='center',
+        margin_top="15em"
+    )
 
 
 app = rx.App()
